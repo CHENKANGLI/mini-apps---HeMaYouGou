7a1476f95030893543edc098bc8a3503d9efd64e
陈康力 <1141132475@qq.com>
2020-02-29 01:12:14 +0800

    tabBar配置、首页轮播图、导航菜单的静态部分完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,38 @@
+import request from "../../utils/request.js"
 Page({
+  data:{
+    // 轮播图数据
+    swiperImg: [],
+    indicatorDots: true,
+    indicatorColor: 'rgba(225,225,225,.3)',
+    indicatorActiveColor: "#fff",
+    autoplay: true,
+    interval: 2000,
+    duration:500,
+    circular:true,
+    // 导航菜单数据
+    menus:[]
+  },
+  onLoad(){
+    // 加载轮播图数据
+    request({
+      url:'/home/swiperdata'
+    }).then(res=>{
+      // console.log(res)
+      const {message} = res.data
+      this.setData({
+        swiperImg:message,
+      })
+    })
+    // 导航菜单
+    request({
+      url:'/home/catitems'
+    }).then(res=>{
+      // console.log(res)
+      const { message } = res.data
+      this.setData({
+        menus: message,
+      })
+    })
+  }
 })
