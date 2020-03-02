672e424bbc0b50d3eae2757c08504d7e18df7cfb
陈康力 <1141132475@qq.com>
2020-03-03 00:50:50 +0800

    商品列表静态布局完成

--- pages/goods_list/goods_list.js
+++ pages/goods_list/goods_list.js
@@ -1,11 +1,13 @@
-// pages/goods_list/goods_list.js
+import request from "../../utils/request.js"
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-     keyword:''
+     keyword:'曲面电视',
+     pagenum:1,
+     goods:[]
   },
 
   /**
@@ -13,9 +15,28 @@ Page({
    */
   onLoad(options){
     console.log(options)
-    const { keyword } = options
-     this.setData({
-       keyword:keyword
+    // const { keyword } = options
+    //  this.setData({
+    //    keyword:keyword
+    //  })
+
+     request({
+       url:"/goods/search",
+       data:{
+         query:this.data.keyword,
+         pagenum:this.data.pagenum,
+         pagesize:10
+       }
+     }).then(res=>{
+      //  console.log(res)
+      const {message}=res.data
+      const goods=message.goods.map((v,i)=>{
+        v.goods_price=Number(v.goods_price).toFixed(2)
+        return v
+      })
+      this.setData({
+        goods:[...this.data.goods,...goods]
+      })
      })
   },
 
